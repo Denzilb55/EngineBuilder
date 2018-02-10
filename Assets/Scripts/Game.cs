11692c375fc70ee146965a632bc278c4f4e3ea2a
Ruan Moolman <ruan.moolman@gmail.com>
2018-02-10 11:38:12 +0200

    Display player credits

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,21 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
 
-	public Player playerTemplate;
+	public Player playerTemplate;	
+	public Text[] playerTexts;
+
+	private int numPlayers;
 	private Map map;
+
+	private List<Player> players;
 
 	// Use this for initialization
 	void Start () {
+		numPlayers = 1;
 		map = GetComponent<Map>();
-		Player player = GameObject.Instantiate<Player>(playerTemplate);
-		player.cursor.map = map;
+		players = new List<Player>();
+		for (int i = 0; i < numPlayers; i++) {
+			Player player = GameObject.Instantiate<Player>(playerTemplate);
+			players.Add(player);
+			player.cursor.map = map;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		for (int i = 0; i < numPlayers; i++) {
+			playerTexts[i].text = players[i].credits.ToString();
+		}
 	}
 }
