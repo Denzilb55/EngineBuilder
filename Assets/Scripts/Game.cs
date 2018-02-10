e809e14111d417451d09f9c34a90e3df5db1dc6a
Ruan Moolman <ruan.moolman@gmail.com>
2018-02-10 11:24:59 +0200

    Added Player to Game object

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+	public Player playerTemplate;
 	private Map map;
 
 	// Use this for initialization
 	void Start () {
 		map = GetComponent<Map>();
+		Player player = GameObject.Instantiate<Player>(playerTemplate);
+		player.cursor.map = map;
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int credits {
+		get;
+		private set;
+	}
+
+	public Cursor cursor {
+		get {return GetComponent<Cursor>();}
+	}
+
+	// Use this for initialization
+	void Start () {
+		credits = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
